f8455c013676718b4ea95f1de6f8bfe31b75ba62
EpameinondasTheodoro <EpameinondasTheodoro@DESKTOP-9P1KD0D>
2019-11-22 13:22:14 +0200

    added modal

--- src/app/places/discover/place-detail/place-detail.module.ts
+++ src/app/places/discover/place-detail/place-detail.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { PlaceDetailPage } from './place-detail.page';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,8 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [PlaceDetailPage]
+  declarations: [PlaceDetailPage, CreateBookingComponent],
+  //Epeidh den to vazoume OUTE me to tag tou OUTE pame e auto me to routing tote prepei na to valoume e ayton ton eidiko array
+  entryComponents: [CreateBookingComponent]
 })
 export class PlaceDetailPageModule {}
--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
 import { Place } from '../../place.model';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -11,7 +12,13 @@ import { Place } from '../../place.model';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router: Router, private navCtrl: NavController, private route: ActivatedRoute, private placesService: PlacesService) { }
+  constructor(
+    private router: Router, 
+    private navCtrl: NavController, 
+    private route: ActivatedRoute, 
+    private placesService: PlacesService,
+    private modalCtrl: ModalController
+    ) { }
 
   place: Place;
   ngOnInit() {
@@ -28,12 +35,34 @@ export class PlaceDetailPage implements OnInit {
 
   onBookPlace()
   {
+    /*
     //this.router.navigateByUrl('/places/tabs/discover');
 
     // h diafora me to button pou valame me ta component ths ionic einai oti ekeino xerei na pexei ta swsta animations
     // auto edw den xerei. Opote me to navigateBack xerei ti na paixei san animation
     this.navCtrl.navigateBack('/places/tabs/discover'); //tha xrisimopoihsei to angular router 
     //this.navCtrl.pop() //vgazei thn teleutaia page apo to stack. An kaneis reload omws xekinaei apo mia selida sto stack kai den xerei pou na paei
+
+    */
+
+    // Pio prin mexri na ftasoume na ftiaxoume to modal eixame to apo panw poy den ekane tipota. Ftasame sth enotita tou modal twra
+    this.modalCtrl
+    .create({
+      component: CreateBookingComponent, 
+      componentProps: {selectedPlace: this.place}
+    })
+    .then(modalEl => {
+      modalEl.present();
+      return modalEl.onDidDismiss(); //gyrnaei ena allo promise opote mporoume na kanoume chain ta then
+    })
+    .then(resultData => {
+      console.log(resultData.data, resultData.role);
+      if(resultData.role === 'confirm')
+      {
+        console.log("BOOKED!!");
+      }
+    });
+
   }
 
 }

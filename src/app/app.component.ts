1529b6a1cc9406f0860c4b2dda72c16d4e1837d2
EpameinondasTheodoro <EpameinondasTheodoro@DESKTOP-9P1KD0D>
2019-12-11 15:22:39 +0200

    Geolocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+// import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+// import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins, Capacitor } from '@capacitor/core';
+
 import { AuthService } from './auth/auth.service';
 import { Router } from '@angular/router';
 
@@ -12,9 +14,10 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   constructor(
+    // sou epitrepei na katalaveis sto platform pou trexei to app sou ,an einai etoimo
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
+  //  private splashScreen: SplashScreen, // einai apo cordova opote den ta xrisimopoioume edw
+  //  private statusBar: StatusBar, // einai apo cordova opote den ta xrisimopoioume edw 
     private authService: AuthService,
     private router: Router
   ) {
@@ -22,9 +25,17 @@ export class AppComponent {
   }
 
   initializeApp() {
+ //   this.platform // me tis epiloges pou exei mporeis na deis to dimension
+  //  this. platform.is('Android') // mporei na deis se ti platforma trexei to app sou
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+
+      if(Capacitor.isPluginAvailable('SplashScreen'))
+      {
+        Plugins.SplashScreen.hide();// automatically shows when the application boots
+      }
+
+      // this.statusBar.styleDefault();
+      // this.splashScreen.hide();
     });
   }
 
--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ActionSheetController, AlertController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { map, switchMap } from 'rxjs/operators';
-import { OfferLocation } from 'src/app/places/location.model';
+import { OfferLocation, Coordinates } from 'src/app/places/location.model';
 import { of } from 'rxjs';
+import { Plugins, Capacitor } from '@capacitor/core';
+
 
 @Component({
   selector: 'app-location-picker',
@@ -18,11 +20,101 @@ export class LocationPickerComponent implements OnInit {
   isLoading = false;
   @Output() locationPick = new EventEmitter<OfferLocation>();
 
-  constructor(private modalCtrl: ModalController, private http: HttpClient) { }
+  constructor(
+    private modalCtrl: ModalController, 
+    private http: HttpClient, 
+    private actionSheetCtrl: ActionSheetController,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {}
 
   onPickLocation()
+  {
+    this.actionSheetCtrl.create({
+      header: 'Please Choose',
+      buttons: [
+        {text: 'Auto-Locate', handler: () => {
+          this.locateUser();
+        }},
+        {text: 'Pick on Map', handler: () => {
+          this.openMap();
+        }},
+        {text: 'Cancel', role: 'cancel'},
+      ]
+    })
+    .then(actionSheetEl => {
+      actionSheetEl.present();
+    });
+
+   
+  }
+
+  private showErrorAlert()
+  {
+    this.alertCtrl.create({
+      header: 'Could do not fetch location',
+      message: 'Please use the map to pic a location!',
+      buttons: ['Okay']
+    })
+    .then(alertel => {
+      alertel.present();
+    });
+  }
+
+  private locateUser()
+  {
+    if(!Capacitor.isPluginAvailable('Geolocation'))
+    {
+      this.showErrorAlert();
+      return;
+    }
+    this.isLoading = true;
+    debugger;
+    Plugins.Geolocation.getCurrentPosition()
+    .then(geoPosition => {
+      const coordinates: Coordinates = {
+        lat: geoPosition.coords.latitude, 
+        lng:geoPosition.coords.longitude 
+      };
+      this.createPlace(coordinates.lat, coordinates.lng);
+      this.isLoading = false;
+    })
+    .catch(err => {
+      this.isLoading = false;
+      this.showErrorAlert();
+    });
+  }
+
+
+  private createPlace(lat: number, lng:number)
+  {
+    const pickedLocation: OfferLocation = {
+      lat: lat,
+      lng: lng,
+      address: null,
+      staticMapImageUrl: null
+    };
+    this.isLoading = true;
+    this.getAddress(lat, lng)
+    .pipe(
+      switchMap( address => {
+        pickedLocation.address = address;
+        return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+      })
+      ).subscribe(staticMapImageUrl => {
+        pickedLocation.staticMapImageUrl = staticMapImageUrl;
+        this.selectedLocationImage = staticMapImageUrl;
+        this.isLoading = false;
+        this.locationPick.emit(pickedLocation);
+      });
+              // to kaname gia na doume thn dieuthinsi
+        // .subscribe(
+        //   (address) => {
+        //     console.log('Address: ', address);
+        // });
+  }
+
+  private openMap()
   {
     this.modalCtrl.create({component: MapModalComponent})
     .then(modalEl => {
@@ -34,30 +126,14 @@ export class LocationPickerComponent implements OnInit {
           return;
         }
 
-        const pickedLocation: OfferLocation = {
+        const coordinates: Coordinates = {
           lat: modalData.data.lat,
-          lng: modalData.data.lng,
-          address: null,
-          staticMapImageUrl: null
+          lng: modalData.data.lng
         };
-        this.isLoading = true;
-        this.getAddress(modalData.data.lat, modalData.data.lng)
-        .pipe(
-          switchMap( address => {
-            pickedLocation.address = address;
-            return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
-          })
-          ).subscribe(staticMapImageUrl => {
-            pickedLocation.staticMapImageUrl = staticMapImageUrl;
-            this.selectedLocationImage = staticMapImageUrl;
-            this.isLoading = false;
-            this.locationPick.emit(pickedLocation);
-          });
-        // to kaname gia na doume thn dieuthinsi
-        // .subscribe(
-        //   (address) => {
-        //     console.log('Address: ', address);
-        // });
+
+        this.createPlace(coordinates.lat, coordinates.lng); // to valame se mia xehoristi methodo olh thn diadikasia gia na einai pio wraio alla giati to xrisimopoioume kai sto autolocation pio panw
+
+
       });
       modalEl.present();
     });
